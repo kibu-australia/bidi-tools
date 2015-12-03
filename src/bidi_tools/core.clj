c907a095375ab86fa0b274211f042da1b5cd6109
Thomas Crowley <crowley@kibu.com.au>
2015-12-03 19:24:15 +1100

    Initial commit

---
+++ src/bidi_tools/core.clj
@@ -0,0 +1,6 @@
+(ns bidi-tools.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/bidi_tools/core_test.clj
@@ -0,0 +1,7 @@
+(ns bidi-tools.core-test
+  (:require [clojure.test :refer :all]
+            [bidi-tools.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
