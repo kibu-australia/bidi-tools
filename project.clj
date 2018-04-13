9e3b71f7a1f86011524dd289ea94ae2708de6ab5
Thomas Crowley <crowley@kibu.com.au>
2018-04-13 14:07:45 +1000

    bump minor

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kibu/bidi-tools "0.3.0"
+(defproject kibu/bidi-tools "0.3.1"
   :description "Tools for bidi"
   :url "https://github.com/kibu-australia/bidi-tools"
   :license {:name "Eclipse Public License"
@@ -6,4 +6,6 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.cemerick/url "0.1.1"]
                  [bidi "1.21.1" :scope "provided"]
-                 [prismatic/schema "1.0.4"]])
+                 [prismatic/schema "1.0.4"]]
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]])
